f2fd140a1dbc199d5e09cc6b73296d743842cd7f
Admin <241538nxdd@gmail.com>
2020-10-19 19:39:15 +0700

    Bai 13 Handling events

--- src/App.js
+++ src/App.js
@@ -5,21 +5,52 @@ import TodoItem from './components/TodoItem';
 class App extends Component {
   constructor() {
     super();
-    this.todoItems = [
+    this.state = {
+      todoItems: [
       { title: 'Mua bim bim', isComplete: true },
       { title: 'Di da bong', isComplete: true },
       { title: 'Di do xang' }
-    ];
+      ]
+    }
   }
-  render(){
+  onItemClicked(item){
+    return (event) => {
+      const isComplete = item.isComplete;
+      const { todoItems } = this.state;
+      const index = todoItems.indexOf(item);
+      this.setState({
+        todoItems: [
+          ...todoItems.slice(0, index),
+          {
+            ...item,
+            isComplete: !isComplete
+          },
+          ...todoItems.slice(index + 1)
+        ]
+      })
+    };
+  }
+
+
+  render() {
+    const { todoItems } = this.state;
+    if (todoItems.length) {
     return (
       <div className="App">
-        {this.todoItems.length > 0 && this.todoItems.map((item, index) => (
-            <TodoItem key={index} item={item} />
-          ))}
-        {this.todoItems.length === 0 && 'Nothing here...'}
-      </div>
+        { todoItems.length && todoItems.map((item, index) =>
+            <TodoItem
+              key={index}
+              item={item}
+              onClick={this.onItemClicked(item)} />
+        )
+      }
+    </div>
     );
+    } else {
+      return (
+        <div className = "App">Nothing here.</div>
+      );
+    }
   }
 }
 
