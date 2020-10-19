9aec7aa8384be06f922456808cea2e44b56157e7
Admin <241538nxdd@gmail.com>
2020-10-19 21:16:43 +0700

    Bai 16 TODO App part 2

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,15 +1,23 @@
 import React, { Component } from 'react';
 import classNames from 'classnames';
 import './TodoItem.css';
+import checkImg from '../img/tick.svg';
+import checkCompleteImg from '../img/tick1.svg';
 
 class TodoItem extends Component {
     render() {
         const { item, onClick } = this.props;
+        let url = checkImg;
+        if (item.isComplete) {
+            url = checkCompleteImg;
+        }
 
         return (
-            <div onClick={onClick} className={classNames('TodoItem', {
+            <div className={classNames('TodoItem', {
                 'TodoItem-complete': item.isComplete
             })}>
+            
+            <img onClick={onClick} src={url} width={32}/>
             <p>{this.props.item.title}</p>
             </div>
         );
